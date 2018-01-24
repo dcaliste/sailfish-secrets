53bfbe3a494d35c067cc2205e3fe54083e1dd90d
Chris Adams <chris.adams@jollamobile.com>
2018-01-24 11:10:39 +1000

    [sailfish-secrets] Add framework overview documentation. Contributes to JB#36797
    This commit adds documentation for the Sailfish OS Secrets and Crypto
    Framework, including overview documentation for the Sailfish OS Secrets
    Library and the Sailfish OS Crypto Library.
    
    It also changes the install path of C++ plugins and devel headers
    for symmetry with the install path for QML plugins and documentation.
    
    Contributes to JB#36797

---
+++ lib/Crypto/doc/sailfish-crypto.cpp
@@ -0,0 +1,17 @@
+namespace Sailfish {
+namespace Crypto {}
+}
+
+/*!
+  \namespace Sailfish
+  \brief This namespace provides various APIs to interact with the Sailfish OS system.
+ */
+
+using namespace Sailfish;
+
+/*!
+  \namespace Crypto
+  \brief This namespace provides various types related to performing cryptographic operations in the Sailfish OS system.
+ */
+
+using namespace Sailfish::Crypto;
---
+++ lib/Secrets/doc/sailfish-secrets.cpp
@@ -0,0 +1,17 @@
+namespace Sailfish {
+namespace Secrets {}
+}
+
+/*!
+  \namespace Sailfish
+  \brief This namespace provides various APIs to interact with the Sailfish OS system.
+ */
+
+using namespace Sailfish;
+
+/*!
+  \namespace Secrets
+  \brief This namespace provides various types related to storing secret data in the Sailfish OS system.
+ */
+
+using namespace Sailfish::Secrets;
